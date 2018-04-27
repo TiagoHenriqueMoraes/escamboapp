e9e6b10734351bb01802414ce64db2675a86d537
TiagoHenriqueMoraes <tiago_h_moraes@hotmail.com>
2018-04-27 16:10:18 -0300

    Created Categories to admin - added new routes

---
+++ app/controllers/backoffice/categories_controller.rb
@@ -0,0 +1,18 @@
+class Backoffice::CategoriesController < BackofficeController
+  def index
+    @categories = Category.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+  
+  def edit 
+  end
+  
+  def update
+  end
+
+end
