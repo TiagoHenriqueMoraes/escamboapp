ebf1c216c0de7e044795c003432337246c7b603c
TiagoHenriqueMoraes <tiago_h_moraes@hotmail.com>
2018-04-27 17:29:20 -0300

    Added Edit - Dry Controller

--- app/controllers/backoffice/categories_controller.rb
+++ app/controllers/backoffice/categories_controller.rb
@@ -1,4 +1,6 @@
 class Backoffice::CategoriesController < BackofficeController
+  before_action :set_category, only: [:edit, :update]
+
   def index
     @categories = Category.all
   end
@@ -20,6 +22,12 @@ class Backoffice::CategoriesController < BackofficeController
   end
   
   def update
+    if @category.update(params_category)
+      redirect_to backoffice_categories_path, notice: "Update Successful."
+    else
+      render :edit
+    end
+
   end
 
 
@@ -29,4 +37,9 @@ private
   def params_category
     params.require(:category).permit(:description)
   end
+  
+  def set_category
+    @category = Category.find(params[:id])  
+  end
+  
 end
