688ab663b99df86ba77d08afc5f10f1ef1a96066
TiagoHenriqueMoraes <tiago_h_moraes@hotmail.com>
2018-05-03 14:28:59 -0300

    Added Mailer

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -6,4 +6,12 @@ class AdminMailer < ApplicationMailer
         @admin = admin
         mail(to: @admin.email, subject: "Your informations was changed!")
     end
+
+    def send_message(current_admin, recipient, subject, message) 
+        @current_admin = current_admin
+        @recipient = recipient 
+        @subject = subject
+        @message = message
+        mail(to: @recipient, subject: @subject)
+    end
 end
